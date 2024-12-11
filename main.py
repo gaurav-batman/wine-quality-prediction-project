a7ba55191446ee18baab182bd3fa55b4155934d4
GauravPahwa143 <gauravpahwa2020@gmail.com>
2024-12-11 17:00:28 +0530

    data validation stage added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.wine_quality_predictor import logger
 from src.wine_quality_predictor.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.wine_quality_predictor.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -11,3 +12,14 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   obj = DataValidationTrainingPipeline()
+   obj.initiate_data_validation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+   logger.exception(e)
+   raise e
