d6ef12d310f89371d1871f5628c62e1c4bbe1847
GauravPahwa143 <gauravpahwa2020@gmail.com>
2024-12-12 16:45:29 +0530

    data transformation stage added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.wine_quality_predictor import logger
 from src.wine_quality_predictor.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.wine_quality_predictor.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
-
+from src.wine_quality_predictor.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -19,6 +19,16 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    obj = DataValidationTrainingPipeline()
    obj.initiate_data_validation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+   logger.exception(e)
+   raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   obj = DataTransformationTrainingPipeline()
+   obj.initiate_data_transformation()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
    logger.exception(e)
